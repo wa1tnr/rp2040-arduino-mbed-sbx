a1db93cb638664e5132d78779cf01fc9a4f806b5
wa1tnr <wa1tnr@users.noreply.github.com>
2021-07-05 07:01:04 +0000

    valid print BEACON

--- testing/test-a/test-a.ino
+++ testing/test-a/test-a.ino
@@ -1,9 +1,38 @@
-// test-a.ino 05 Jul 2021 05:52z
+// test-a.ino
+// Mon Jul  5 06:53:22 UTC 2021
 
-// bbb
-void waitloop(void) {}
+// status: blinks visibly at least one time. LED builtin remains ON.
+
+void waitloop(void) {
+    for (volatile int i=1800000;i>0;i--); // do not optimize
+}
+
+void led_foo(void) {
+    waitloop(); digitalWrite(LED_BUILTIN, 1); waitloop(); waitloop(); digitalWrite(LED_BUILTIN, 0); waitloop(); waitloop(); digitalWrite(LED_BUILTIN, 1);
+}
+
+void print_beacon(void) { // continuous message
+    Serial.print("  CC");
+}
+
 void setup() {
+    pinMode(LED_BUILTIN, 1);
+    digitalWrite(LED_BUILTIN, 0);
+
     Serial.begin(9600);
-    waitloop();
+
+    waitloop(); digitalWrite(LED_BUILTIN, 1); waitloop(); waitloop(); digitalWrite(LED_BUILTIN, 0); waitloop(); waitloop(); digitalWrite(LED_BUILTIN, 1);
+
+    waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop();
+    waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop();
+    waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop();
+    waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop();
+    Serial.println("This is a serial println message.");
+    Serial.print("\n\n  AA");
+    Serial.print("  BB");
+    do {
+        print_beacon();
+        waitloop(); waitloop(); waitloop(); waitloop();
+    } while (1);
 }
 void loop() { while (1); }
