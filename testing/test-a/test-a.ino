761a207675cf6cd058bead4e875f73717200aeeb
wa1tnr <wa1tnr@users.noreply.github.com>
2021-07-05 07:11:29 +0000

    blink LED in time with print beacon - LONG wait.

--- testing/test-a/test-a.ino
+++ testing/test-a/test-a.ino
@@ -1,37 +1,41 @@
 // test-a.ino
-// Mon Jul  5 06:53:22 UTC 2021
+// Mon Jul  5 07:03:40 UTC 2021
 
-// status: blinks visibly at least one time. LED builtin remains ON.
+// status: Prints continuously.
+// Blinks visibly at least one time. LED builtin remains ON.
 
 void waitloop(void) {
     for (volatile int i=1800000;i>0;i--); // do not optimize
 }
 
+void longwaitloop(void){for (volatile int i=27;i>0;i--){ waitloop();}}
+
 void led_foo(void) {
     waitloop(); digitalWrite(LED_BUILTIN, 1); waitloop(); waitloop(); digitalWrite(LED_BUILTIN, 0); waitloop(); waitloop(); digitalWrite(LED_BUILTIN, 1);
 }
 
+uint8_t count = 0;
 void print_beacon(void) { // continuous message
-    Serial.print("  CC");
+    count++;
+    Serial.print("  BEACON ");
+    Serial.print(count);
 }
 
 void setup() {
     pinMode(LED_BUILTIN, 1);
     digitalWrite(LED_BUILTIN, 0);
-
+    longwaitloop();
+    digitalWrite(LED_BUILTIN, 1);
+    longwaitloop();
+    digitalWrite(LED_BUILTIN, 0);
+    longwaitloop();
     Serial.begin(9600);
+    longwaitloop();
+    longwaitloop();
 
-    waitloop(); digitalWrite(LED_BUILTIN, 1); waitloop(); waitloop(); digitalWrite(LED_BUILTIN, 0); waitloop(); waitloop(); digitalWrite(LED_BUILTIN, 1);
-
-    waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop();
-    waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop();
-    waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop();
-    waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop(); waitloop();
-    Serial.println("This is a serial println message.");
-    Serial.print("\n\n  AA");
-    Serial.print("  BB");
     do {
         print_beacon();
+        led_foo();
         waitloop(); waitloop(); waitloop(); waitloop();
     } while (1);
 }
